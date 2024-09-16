effaf51e0434ab543015643a8dee042ee9155750
Manuel Stähli <manuel.staehli@itesys.ch>
2024-09-16 23:32:59 +0200

    title highlight

--- resources/js/Layouts/Header.vue
+++ resources/js/Layouts/Header.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="row flex-center">
         <div class="col col-8">
-            <h2 class="m-1" align="center">Read the fucking error message!!!</h2>
+            <h2 class="m-1" align="center"><span class="background-primary border border-primary">R</span>ead
+                <span class="background-primary border border-primary">T</span>he <span
+                    class="background-primary border border-primary">F</span>ucking
+                <span class="background-primary border border-primary">E</span>rror
+                <span class="background-primary border border-primary">M</span>essage!!!
+            </h2>
         </div>
     </div>
 </template>
